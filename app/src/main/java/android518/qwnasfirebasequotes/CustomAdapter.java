e9723e1fa38cc7556cacff1e18c254fa1938fa9c
ashulzhenko <sh.eileen@outlook.com>
2016-10-30 18:46:41 -0400

    added more design for MainActivity

--- app/src/main/java/android518/qwnasfirebasequotes/CustomAdapter.java
+++ app/src/main/java/android518/qwnasfirebasequotes/CustomAdapter.java
@@ -1,10 +1,8 @@
 package android518.qwnasfirebasequotes;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
