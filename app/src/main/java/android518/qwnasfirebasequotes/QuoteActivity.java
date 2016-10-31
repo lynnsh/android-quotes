f69683c90a4914ed385fe7e1ed2e6a72ce8f15d8
WillyNgo <ngo.willi@gmail.com>
2016-10-31 15:48:19 -0400

    quoteActivity works, layout has been done

--- app/src/main/java/android518/qwnasfirebasequotes/QuoteActivity.java
+++ app/src/main/java/android518/qwnasfirebasequotes/QuoteActivity.java
@@ -1,10 +1,12 @@
 package android518.qwnasfirebasequotes;
 
 import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -28,30 +30,42 @@ public class QuoteActivity extends AppCompatActivity {
         displayQuote(q);
     }
 
-    private void displayQuote(Quote quote)
+    private void displayQuote(final Quote quote)
     {
         TextView attr_textview = (TextView) findViewById(R.id.attributedTextView);
         TextView quote_textview = (TextView) findViewById(R.id.quoteTextView);
         TextView ref_textview = (TextView) findViewById(R.id.referenceTextView);
         TextView date_textview = (TextView) findViewById(R.id.dateTextView);
 
-        attr_textview.setText(quote.getAttributed());
+        attr_textview.setText("- " + quote.getAttributed());
         quote_textview.setText(quote.getQuote());
         ref_textview.setText(quote.getReference());
         date_textview.setText(quote.getDate());
+
+        //Add onClick
+        attr_textview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayBlurb(quote);
+            }
+        });
     }
 
-    private void displayBlurb()
+    private void displayBlurb(Quote quote)
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        builder.setTitle(q.getAttributed())
-                .setMessage(q.getBlurb())
-                .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+        builder.setTitle(quote.getAttributed())
+                .setMessage(quote.getBlurb())
+                .setCancelable(true)
+                .setNegativeButton(R.string.dismiss, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        //take out alert
+                        //Do nothing, just dismiss the dialog
                     }
                 });
+
+        AlertDialog ad = builder.create();
+        ad.show();
     }
 }
