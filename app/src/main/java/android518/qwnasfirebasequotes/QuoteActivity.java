08bfcef7818587c06ab5c7927db28660873a1c45
WillyNgo <ngo.willi@gmail.com>
2016-11-04 21:29:28 -0400

    fixed name disappearing when rotating about page, add try/catch clause to displayQuote, added clickable link to reference

--- app/src/main/java/android518/qwnasfirebasequotes/QuoteActivity.java
+++ app/src/main/java/android518/qwnasfirebasequotes/QuoteActivity.java
@@ -1,10 +1,13 @@
 package android518.qwnasfirebasequotes;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -30,27 +33,62 @@ public class QuoteActivity extends AppCompatActivity {
         displayQuote(q);
     }
 
+    /**
+     * Displays all information pertaining to a quote
+     *
+     * @param quote
+     */
     private void displayQuote(final Quote quote)
     {
-        TextView attr_textview = (TextView) findViewById(R.id.attributedTextView);
-        TextView quote_textview = (TextView) findViewById(R.id.quoteTextView);
-        TextView ref_textview = (TextView) findViewById(R.id.referenceTextView);
-        TextView date_textview = (TextView) findViewById(R.id.dateTextView);
+        try {
 
-        attr_textview.setText("- " + quote.getAttributed());
-        quote_textview.setText("\" " + quote.getQuote() + " \"");
-        ref_textview.setText(quote.getReference());
-        date_textview.setText(quote.getDate());
+            TextView attr_textview = (TextView) findViewById(R.id.attributedTextView);
+            TextView quote_textview = (TextView) findViewById(R.id.quoteTextView);
+            TextView ref_textview = (TextView) findViewById(R.id.referenceTextView);
+            TextView date_textview = (TextView) findViewById(R.id.dateTextView);
 
-        //Add onClick
-        attr_textview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayBlurb(quote);
-            }
-        });
+            attr_textview.setText("- " + quote.getAttributed());
+            quote_textview.setText("\" " + quote.getQuote() + " \"");
+            ref_textview.setText(quote.getReference());
+            date_textview.setText(quote.getDate());
+
+            //Linkify reference to allow user to open browser to the webpage
+            Linkify.addLinks(ref_textview, Linkify.WEB_URLS);
+
+            //Allow user to view the blurb by clicking on the attributed's name
+            attr_textview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    displayBlurb(quote);
+                }
+            });
+
+
+
+            /*Add onClick event to open browser to the page of the reference of the quote
+            ref_textview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String url = quote.getReference();
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                }
+            });
+            */
+        }
+        catch(NullPointerException npe)
+        {
+            npe.getMessage();
+        }
     }
 
+    /**
+     * Displays a dialog box containing a blurb which is
+     * information about the attributed person who said the quote
+     *
+     * @param quote - Quote object
+     */
     private void displayBlurb(Quote quote)
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
